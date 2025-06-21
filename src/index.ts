1adc8830bfb8d0e02da8a5aadf5ccb66baeb4ad9
Taiki Nakaoka <nakaokataiki@gmail.com>
2025-06-21 12:59:18 +0900

    型によるドメインモデリングの実装を追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+import * as v from 'valibot';
+
+// 第5 章 型によるドメインモデリング
+const title = '第5 章 型によるドメインモデリング';
+
+// 5.3 単純な値のモデリング
+const customerIdSchema = v.pipe(v.number(), v.brand('CustomerId'));
+type CustomerId = v.InferOutput<typeof customerIdSchema>;
+const orderIdSchema = v.pipe(v.number(), v.brand('OrderId'));
+type OrderId = v.InferOutput<typeof orderIdSchema>;
+
+
+function main() {
+    const customerId = v.safeParse(customerIdSchema, 123);
+    const orderId = v.safeParse(orderIdSchema, 456);
+    
+    console.log("Customer ID:", customerId.output);
+    console.log("Order ID:", orderId.output);
+}
+
+console.log(title);
+main();
